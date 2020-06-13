867355b834cce88ecb2bc0d96357beb958a2d5ff
Gabriel Lisaca <gabriel.lisaca@gmail.com>
2020-06-13 21:52:03 +1200

    Add types to validators (and parsers)
    The type for `list_of` is gonna be a pain.  No thanks.  I'll just let it
    be that imprecise type.

--- surfraw_tools/validation.py
+++ surfraw_tools/validation.py
@@ -1,4 +1,5 @@
 import re
+from typing import Any, Callable, List, TypeVar
 
 
 class OptionParseError(Exception):
@@ -13,7 +14,7 @@ class OptionParseError(Exception):
 _VALID_SURFRAW_VAR_NAME = re.compile("^[a-z]+$")
 
 
-def validate_name(name):
+def validate_name(name: str) -> str:
     if not _VALID_SURFRAW_VAR_NAME.fullmatch(name):
         raise OptionParseError(
             f"name '{name}' is an invalid variable name for an elvis"
@@ -31,7 +32,7 @@ _FALSE_WORDS = {"no"}
 _BOOL_WORDS = _TRUE_WORDS | _FALSE_WORDS
 
 
-def validate_bool(bool_):
+def validate_bool(bool_: str) -> str:
     if bool_ not in _BOOL_WORDS:
         valid_bools = ", ".join(sorted(_BOOL_WORDS))
         raise OptionParseError(
@@ -40,7 +41,7 @@ def validate_bool(bool_):
     return bool_
 
 
-def parse_bool(bool_):
+def parse_bool(bool_: str) -> bool:
     if bool_ in _TRUE_WORDS:
         return True
     elif bool_ in _FALSE_WORDS:
@@ -60,7 +61,7 @@ _VALID_ENUM_VALUE_STR = "^[a-z0-9][a-z0-9_+-]*$"
 _VALID_ENUM_VALUE = re.compile(_VALID_ENUM_VALUE_STR)
 
 
-def validate_enum_value(value):
+def validate_enum_value(value: str) -> str:
     if not _VALID_ENUM_VALUE.fullmatch(value):
         raise OptionParseError(
             f"enum value '{value}' must match the regex '{_VALID_ENUM_VALUE_STR}'"
@@ -70,12 +71,14 @@ def validate_enum_value(value):
 
 # MISC.
 
+T = TypeVar("T")
 
-def no_validation(arg):
+
+def no_validation(arg: T) -> T:
     return arg
 
 
-def list_of(validator):
+def list_of(validator: Callable[..., Any]) -> Callable[..., List[Any]]:
     def list_validator(arg):
         values = arg.split(",")
         # In case the validators return a different object from its input.
