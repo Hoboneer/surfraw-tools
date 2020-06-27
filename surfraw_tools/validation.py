838a3c3e12c3a6a077a09a7d2f72b43445f7a627
Gabriel Lisaca <gabriel.lisaca@gmail.com>
2020-06-27 18:19:07 +1200

    Add docstrings to `validation.py`

--- surfraw_tools/validation.py
+++ surfraw_tools/validation.py
@@ -1,3 +1,7 @@
+"""Validators and parsers for option arguments.
+
+All functions should raise `OptionParseError` on invalid input.
+"""
 from __future__ import annotations
 
 import re
@@ -8,11 +12,11 @@ if TYPE_CHECKING:
 
 
 class OptionParseError(Exception):
-    pass
+    """Exception when options are given incorrect arguments."""
 
 
 class OptionResolutionError(Exception):
-    pass
+    """Exception when resolving options' targets to concrete objects fails."""
 
 
 # NAME
@@ -24,6 +28,10 @@ _VALID_SURFRAW_VAR_NAME: Final = re.compile("^[a-z]+$")
 
 
 def validate_name(name: str) -> str:
+    """Return `name` unchanged if it is valid for inclusion in elvi.
+
+    Raises `OptionParseError` on invalid input.
+    """
     if not _VALID_SURFRAW_VAR_NAME.fullmatch(name):
         raise OptionParseError(
             f"name '{name}' is an invalid variable name for an elvis"
@@ -42,6 +50,10 @@ _BOOL_WORDS: Final = _TRUE_WORDS | _FALSE_WORDS
 
 
 def validate_bool(bool_: str) -> str:
+    """Return `bool_` unchanged if it is a word representing a boolean.
+
+    Raises `OptionParseError` on invalid input.
+    """
     if bool_ not in _BOOL_WORDS:
         valid_bools = ", ".join(sorted(_BOOL_WORDS))
         raise OptionParseError(
@@ -51,6 +63,10 @@ def validate_bool(bool_: str) -> str:
 
 
 def parse_bool(bool_: str) -> bool:
+    """Map boolean words to `True` or `False`.
+
+    Raises `OptionParseError` on invalid input.
+    """
     if bool_ in _TRUE_WORDS:
         return True
     elif bool_ in _FALSE_WORDS:
@@ -71,6 +87,13 @@ _VALID_ENUM_VALUE: Final = re.compile(_VALID_ENUM_VALUE_STR)
 
 
 def validate_enum_value(value: str) -> str:
+    """Return `value` unchanged if it is valid for surfraw enums.
+
+    Technically, anything is valid for surfraw enums, but *our* enums are
+    restricted since it makes life a bit easier.
+
+    Raises `OptionParseError` on invalid input.
+    """
     if not _VALID_ENUM_VALUE.fullmatch(value):
         raise OptionParseError(
             f"enum value '{value}' must match the regex '{_VALID_ENUM_VALUE_STR}'"
@@ -82,6 +105,10 @@ def validate_enum_value(value: str) -> str:
 
 
 def no_validation(arg: str) -> str:
+    """Return `arg` unchanged.
+
+    This is an identity function and raises no exceptions.
+    """
     return arg
 
 
@@ -89,6 +116,8 @@ T = TypeVar("T")
 
 
 def list_of(validator: Callable[[str], T]) -> Callable[[str], List[T]]:
+    """Run `validator` on a comma-delimited list of arguments."""
+
     def list_validator(arg: str) -> List[T]:
         if arg == "":
             return []
