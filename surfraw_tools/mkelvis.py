13a24601209ecf1ee751e55f9a9e44daae5d62b2
Gabriel Lisaca <gabriel.lisaca@gmail.com>
2020-07-02 23:34:56 +1200

    Reorganise `mkelvis --help` output
    Grouping related options makes it much nicer.

--- surfraw_tools/mkelvis.py
+++ surfraw_tools/mkelvis.py
@@ -215,8 +215,17 @@ def _get_parser() -> argparse.ArgumentParser:
         "--description",
         help="description for the elvis, excluding the domain name in parentheses",
     )
-    parser.add_argument(
-        "--insecure", action="store_true", help="use 'http' instead of 'https'"
+    _search_args_group = parser.add_mutually_exclusive_group()
+    _search_args_group.add_argument(
+        "--query-parameter",
+        "-Q",
+        help="define the parameter for the query arguments; needed with --map",
+    )
+    _search_args_group.add_argument(
+        "--no-append-args",
+        action="store_false",
+        dest="append_search_args",
+        help="don't automatically append search to url",
     )
     parser.add_argument(
         "--output",
@@ -225,115 +234,15 @@ def _get_parser() -> argparse.ArgumentParser:
         metavar="FILE",
         help="write elvis code to FILE instead of elvis name",
     )
+    parser.add_argument(
+        "--insecure", action="store_true", help="use 'http' instead of 'https'"
+    )
+    parser.add_argument(
+        "--num-tabs",
+        type=int,
+        help="define the number of tabs after the elvis name in `sr -elvi` output for alignment",
+    )
 
-    # Option generation
-
-    # Include the 'or' for the last typename.
-    parser.add_argument(
-        "--flag",
-        "-F",
-        action="append",
-        type=_wrap_parser(FlagOption.from_arg),
-        dest="unresolved_flags",
-        metavar="FLAG_NAME:FLAG_TARGET:VALUE",
-        help=f"specify an alias to a value(s) of a defined {_VALID_FLAG_TYPES_STR} option",
-    )
-    parser.add_argument(
-        "--yes-no",
-        "-Y",
-        action="append",
-        type=_wrap_parser(BoolOption.from_arg),
-        dest="unresolved_varopts",
-        metavar="VARIABLE_NAME:DEFAULT_YES_OR_NO",
-        help="specify a boolean option for the elvis",
-    )
-    parser.add_argument(
-        "--enum",
-        "-E",
-        action="append",
-        type=_wrap_parser(EnumOption.from_arg),
-        dest="unresolved_varopts",
-        metavar="VARIABLE_NAME:DEFAULT_VALUE:VAL1,VAL2,...",
-        help="specify an option with an argument from a range of values",
-    )
-    parser.add_argument(
-        "--anything",
-        "-A",
-        action="append",
-        dest="unresolved_varopts",
-        type=_wrap_parser(AnythingOption.from_arg),
-        metavar="VARIABLE_NAME:DEFAULT_VALUE",
-        help="specify an option that is not checked",
-    )
-    parser.add_argument(
-        "--alias",
-        action="append",
-        type=_wrap_parser(AliasOption.from_arg),
-        dest="unresolved_aliases",
-        metavar="ALIAS_NAME:ALIAS_TARGET:ALIAS_TARGET_TYPE",
-        help="make an alias to another defined option",
-    )
-    parser.add_argument(
-        "--list",
-        action="append",
-        type=_wrap_parser(ListOption.from_arg),
-        dest="unresolved_varopts",
-        metavar="LIST_NAME:LIST_TYPE:DEFAULT1,DEFAULT2,...[:VALID_VALUES_IF_ENUM]",
-        help="create a list of enum or 'anything' values as a repeatable (cumulative) option (e.g., `-add-foos=bar,baz,qux`)",
-    )
-    parser.add_argument(
-        "--use-results-option",
-        action="store_true",
-        dest="use_results_option",
-        help="define a 'results' variable and option",
-    )
-    parser.add_argument(
-        "--use-language-option",
-        action="store_true",
-        dest="use_language_option",
-        help="define a 'language' variable and option",
-    )
-    parser.add_argument(
-        "--map",
-        action="append",
-        type=_wrap_parser(MappingOption.from_arg),
-        dest="mappings",
-        metavar="VARIABLE_NAME:PARAMETER[:URL_ENCODE?]",
-        help="map a variable to a URL parameter; by default, `URL_ENCODE` is 'yes'",
-    )
-    parser.add_argument(
-        "--list-map",
-        action="append",
-        # Same object, different target
-        type=_wrap_parser(MappingOption.from_arg),
-        dest="list_mappings",
-        metavar="VARIABLE_NAME:PARAMETER[:URL_ENCODE?]",
-        help="map the values of a list variable to multiple URL parameters; by default, `URL_ENCODE` is 'yes'",
-    )
-    parser.add_argument(
-        "--inline",
-        action="append",
-        type=_wrap_parser(InlineOption.from_arg),
-        dest="inlines",
-        metavar="VARIABLE_NAME:KEYWORD",
-        help="map a variable to a keyword in the search query (e.g., `filetype:pdf` or `site:example.com`)",
-    )
-    parser.add_argument(
-        "--list-inline",
-        action="append",
-        type=_wrap_parser(InlineOption.from_arg),
-        dest="list_inlines",
-        metavar="VARIABLE_NAME:KEYWORD",
-        help="map the values of a list variable to multiple keywords in the search query (e.g., `foo bar query filetype:pdf filetype:xml`)",
-    )
-    parser.add_argument(
-        "--collapse",
-        action="append",
-        type=_wrap_parser(CollapseOption.from_arg),
-        dest="collapses",
-        metavar="VARIABLE_NAME:VAL1,VAL2,RESULT:VAL_A,VAL_B,VAL_C,RESULT_D:...",
-        help="change groups of values of a variable to a single value",
-    )
     parser.add_argument(
         "--metavar",
         action="append",
@@ -350,23 +259,119 @@ def _get_parser() -> argparse.ArgumentParser:
         metavar="VARIABLE_NAME:DESCRIPTION",
         help="define a description for an option",
     )
-    parser.add_argument(
-        "--num-tabs",
-        type=int,
-        help="define the number of tabs after the elvis name in `sr -elvi` output for alignment",
+
+    option_group = parser.add_argument_group("elvi option types")
+    option_group.add_argument(
+        "--flag",
+        "-F",
+        action="append",
+        type=_wrap_parser(FlagOption.from_arg),
+        dest="unresolved_flags",
+        metavar="FLAG_NAME:FLAG_TARGET:VALUE",
+        help=f"specify an alias to a value(s) of a defined {_VALID_FLAG_TYPES_STR} option",
     )
-    _search_args_group = parser.add_mutually_exclusive_group()
-    _search_args_group.add_argument(
-        "--query-parameter",
-        "-Q",
-        help="define the parameter for the query arguments; needed with --map",
+    option_group.add_argument(
+        "--yes-no",
+        "-Y",
+        action="append",
+        type=_wrap_parser(BoolOption.from_arg),
+        dest="unresolved_varopts",
+        metavar="VARIABLE_NAME:DEFAULT_YES_OR_NO",
+        help="specify a boolean option for the elvis",
     )
-    _search_args_group.add_argument(
-        "--no-append-args",
-        action="store_false",
-        dest="append_search_args",
-        help="don't automatically append search to url",
+    option_group.add_argument(
+        "--enum",
+        "-E",
+        action="append",
+        type=_wrap_parser(EnumOption.from_arg),
+        dest="unresolved_varopts",
+        metavar="VARIABLE_NAME:DEFAULT_VALUE:VAL1,VAL2,...",
+        help="specify an option with an argument from a range of values",
     )
+    option_group.add_argument(
+        "--anything",
+        "-A",
+        action="append",
+        dest="unresolved_varopts",
+        type=_wrap_parser(AnythingOption.from_arg),
+        metavar="VARIABLE_NAME:DEFAULT_VALUE",
+        help="specify an option that is not checked",
+    )
+    option_group.add_argument(
+        "--alias",
+        action="append",
+        type=_wrap_parser(AliasOption.from_arg),
+        dest="unresolved_aliases",
+        metavar="ALIAS_NAME:ALIAS_TARGET:ALIAS_TARGET_TYPE",
+        help="make an alias to another defined option",
+    )
+    option_group.add_argument(
+        "--list",
+        action="append",
+        type=_wrap_parser(ListOption.from_arg),
+        dest="unresolved_varopts",
+        metavar="LIST_NAME:LIST_TYPE:DEFAULT1,DEFAULT2,...[:VALID_VALUES_IF_ENUM]",
+        help="create a list of enum or 'anything' values as a repeatable (cumulative) option (e.g., `-add-foos=bar,baz,qux`)",
+    )
+    option_group.add_argument(
+        "--use-results-option",
+        action="store_true",
+        dest="use_results_option",
+        help="define a 'results' variable and option",
+    )
+    option_group.add_argument(
+        "--use-language-option",
+        action="store_true",
+        dest="use_language_option",
+        help="define a 'language' variable and option",
+    )
+
+    modify_vars_group = parser.add_argument_group(
+        "variable manipulation options",
+        description="map, inline, or modify elvi variables",
+    )
+    modify_vars_group.add_argument(
+        "--map",
+        action="append",
+        type=_wrap_parser(MappingOption.from_arg),
+        dest="mappings",
+        metavar="VARIABLE_NAME:PARAMETER[:URL_ENCODE?]",
+        help="map a variable to a URL parameter; by default, `URL_ENCODE` is 'yes'",
+    )
+    modify_vars_group.add_argument(
+        "--list-map",
+        action="append",
+        # Same object, different target
+        type=_wrap_parser(MappingOption.from_arg),
+        dest="list_mappings",
+        metavar="VARIABLE_NAME:PARAMETER[:URL_ENCODE?]",
+        help="map the values of a list variable to multiple URL parameters; by default, `URL_ENCODE` is 'yes'",
+    )
+    modify_vars_group.add_argument(
+        "--inline",
+        action="append",
+        type=_wrap_parser(InlineOption.from_arg),
+        dest="inlines",
+        metavar="VARIABLE_NAME:KEYWORD",
+        help="map a variable to a keyword in the search query (e.g., `filetype:pdf` or `site:example.com`)",
+    )
+    modify_vars_group.add_argument(
+        "--list-inline",
+        action="append",
+        type=_wrap_parser(InlineOption.from_arg),
+        dest="list_inlines",
+        metavar="VARIABLE_NAME:KEYWORD",
+        help="map the values of a list variable to multiple keywords in the search query (e.g., `foo bar query filetype:pdf filetype:xml`)",
+    )
+    modify_vars_group.add_argument(
+        "--collapse",
+        action="append",
+        type=_wrap_parser(CollapseOption.from_arg),
+        dest="collapses",
+        metavar="VARIABLE_NAME:VAL1,VAL2,RESULT:VAL_A,VAL_B,VAL_C,RESULT_D:...",
+        help="change groups of values of a variable to a single value",
+    )
+
     return parser
 
 
