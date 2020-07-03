ffd119da83eaafd402ebad78191df69d2b1158f5
Gabriel Lisaca <gabriel.lisaca@gmail.com>
2020-07-03 14:00:30 +1200

    Add copyright info to `test-import-time.sh`

--- test-import-time.sh
+++ test-import-time.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+
+# SPDX-FileCopyrightText: 2020 Gabriel Lisaca <gabriel.lisaca@gmail.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
 # Print cumulative import time.
 env PYTHONPROFILEIMPORTTIME=1 mkelvis -h 2>&1 >/dev/null |
 	tail -n +2 |
